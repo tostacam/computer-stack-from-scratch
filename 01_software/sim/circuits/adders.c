741a9fd0b8de472fe78eb6cefa1162f8720971cf
tostacam <edgar-tostado@hotmail.com>
2026-06-24 23:04:44 -0700

    moving circuits to separate folder

---
+++ 01_software/sim/circuits/adders.c
@@ -0,0 +1,35 @@
+#include "adders.h"
+
+HalfAdder half_adder(bit a, bit b) {
+  HalfAdder h;
+  h.sum = XOR(a, b);
+  h.carry_out = AND(a, b);
+  return h;
+}
+
+FullAdder full_adder(bit a, bit b, bit cin) {
+  HalfAdder h1 = half_adder(a, b);
+  HalfAdder h2 = half_adder(cin, h1.sum);
+ 
+  FullAdder f;
+  f.sum = h2.sum;
+  f.carry_out = OR(h1.carry, h2.carry);
+}
+
+add64_result add64(bus64 a, bus64 b, bit cin) {
+  add64_result r;
+  bit next_carry = cin;
+  FullAdder f;
+
+  for (int i = 0; i < BUS64_WIDTH; ++i) {
+    bit a_i = bus64_getbit(a, i);
+    bit b_i = bus64_getbit(b, i);
+  
+    f = full_adder(a_i, b_i, next_carry);
+    bus64_set(&r, i, f.sum);
+    
+    next_carry = f.carry_out;
+  }
+
+  r.carry_out = next_carry;
+}
---
+++ 01_software/sim/circuits/bus.c
@@ -0,0 +1,17 @@
+#include "bus.h"
+
+bus64 bus64_zero() {
+  bus64 b;
+  for (int i = 0; i < BUS64_WIDTH; i++) {
+    b[i] = 0;    
+  }
+  return b;
+}
+
+bit bus64_getbit(bus64 b, int i) {
+  return b.data[iu];
+}
+
+void bus64_setbit(bus64 *b, int i, bit v) {
+  b->data[i] = v;
+}
