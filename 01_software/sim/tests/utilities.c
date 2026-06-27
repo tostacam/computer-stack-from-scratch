dd8083a51d9dc99f3810e9d8e68f2240a9e2ca23
tostacam <edgar-tostado@hotmail.com>
2026-06-26 22:47:10 -0700

    is_equal for bus64, uint64_t

--- 01_software/sim/tests/utilities.c
+++ 01_software/sim/tests/utilities.c
@@ -9,3 +9,11 @@ bus64 encode_amount(uint64_t n) {
   }
   return r;
 }
+
+bit is_equal_bus_uint(bus64 a, uint64_t n) {
+  uint64_t bus_value = decode_amount(a);
+  if (bus_value == n)
+    return 1;
+  else
+    return 0;
+}
--- 01_software/sim/tests/utilities.h
+++ 01_software/sim/tests/utilities.h
@@ -2,8 +2,11 @@
 #define UTILITIES_H
 
 #include <stdint.h>
+#include "bit.h"
 #include "bus.h"
+#include "decoder.h"
 
 bus64 encode_amount(uint64_t n);
+bit is_equal_bus_uint(bus64 a, uint64_t n);
 
 #endif
