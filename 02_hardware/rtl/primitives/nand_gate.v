777e4d9c4dedc5be92d4a83af6da7cab51c15a7c
tostacam <edgar-tostado@hotmail.com>
2026-06-29 09:32:50 -0700

    small note

--- 02_hardware/rtl/primitives/nand_gate.v
+++ 02_hardware/rtl/primitives/nand_gate.v
@@ -4,6 +4,10 @@ module nand_gate(a, b, out);
   input   b;
   output  out;
 
+  // NAND is the most fundamental gate, however HDL 
+  // already has a native nand() function, just 
+  // writing a representation here.
+  // (same with other gates: and, not, or, nor, xor)
   assign out = ~(a & b);
 
 endmodule
