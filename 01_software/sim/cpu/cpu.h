d2099b8202ff742adf1184d4b5abbaa02f676233
tostacam <edgar-tostado@hotmail.com>
2026-06-30 18:14:25 -0700

    matching RISC-V datapath

--- 01_software/sim/cpu/cpu.h
+++ 01_software/sim/cpu/cpu.h
@@ -2,33 +2,32 @@
 #define CPU_CORE_H
 
 #include "register_file.h"
+#include "decoder_encoder.h"
 #include "program_counter.h"
 #include "alu.h"
+#include "ram.h"
+#include "rom.h"
 
 typedef struct {
+  bit alu_src;
+  bit mem_to_reg;
+  bit reg_write;
+  bit mem_read;
+  bit mem_write;
+  bit branch;
+  bit alu_op[2];
+} control_signals;
+
+typedef struct {
+  ALU             alu;
   program_counter pc;
   register_file   rf;
   ROM64           rom;
   RAM64           ram; 
-} cpu_core;
-
-typedef struct {
-  // privaten implementation
-  cpu_core core;
-
-  // input
-  bus64 input_mem;
-  bus64 instruction;
-  bit reset;
-
-  // output
-  bus64 output;
-  bit write_mem;
-  bus64 address_mem;
-  bus64 program_count;
+  control_signals control;
 } CPU;
-
-#endif
 
 void CPU_init(CPU *cpu, ROM64 *rom);
 void CPU_cycle(CPU *cpu);
+
+#endif
