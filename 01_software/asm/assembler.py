0e0574f8278b31269785ce10a6a39ee6587caba6
tostacam <edgar-tostado@hotmail.com>
2026-07-17 12:20:32 -0700

    assembler init

---
+++ 01_software/asm/assembler.py
@@ -0,0 +1,24 @@
+def read_file():
+  print("read file")
+
+def tokenize():
+  print("tokenize")
+
+def parse():
+  print("parse")
+
+def encode():
+  print("encode")
+
+def write_file():
+  print("write file")
+
+def main():
+  read_file()
+  tokenize()
+  parse()
+  encode()
+  write_file()
+
+if __name__ == "__main__":
+  main()
