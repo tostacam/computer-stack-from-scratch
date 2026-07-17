98dfd54712d84092ac11b5d7cefcb261ea08a35d
tostacam <edgar-tostado@hotmail.com>
2026-07-17 11:38:27 -0700

    removing comments

--- 02_hardware/rtl/memory/rom.sv
+++ 02_hardware/rtl/memory/rom.sv
@@ -15,16 +15,4 @@ end
 localparam ADDR_WIDTH = $clog2(MEM_SIZE);
 assign instruction = memory[address[ADDR_WIDTH+1:2]];
 
-/*
-always_comb begin
-  case (address)
-    64'h0000: instruction = 32'h00500093; // addi x1,x0,5
-    64'h0004: instruction = 32'h00A00113; // addi x2,x0,10
-    64'h0008: instruction = 32'h002081B3; // add  x3,x1,x2
-    64'h000C: instruction = 32'h00302023; // sw   x3,0(x0)
-    default: instruction = 32'h00000013; // nop
-  endcase
-end
-*/
-
 endmodule
