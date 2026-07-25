67c031aee06ddc9da45e8ecc305779411b695605
tostacam <edgar-tostado@hotmail.com>
2026-07-25 08:42:38 -0700

    add control opcodes, alu_op changed to uint8_t

--- 01_software/sim/cpu/cpu.h
+++ 01_software/sim/cpu/cpu.h
@@ -10,15 +10,28 @@
 #include "ram.h"
 #include "rom.h"
 
+enum control_opcodes {
+  OPCODE_RTYPE  = 0b0110011,
+  OPCODE_ITYPE  = 0b0010011,
+  OPCODE_LOAD   = 0b0000011,
+  OPCODE_STORE  = 0b0100011,
+  OPCODE_BRANCH = 0b1100011,
+  OPCODE_LUI    = 0b0110111,
+  OPCODE_AUIPC  = 0b0010111,
+  OPCODE_JAL    = 0b1101111,
+  OPCODE_JALR   = 0b1100111,
+  OPCODE_SYSTEM = 0b1110011
+};
+
 typedef struct {
-  bit alu_src;
-  bit mem_to_reg;
-  bit reg_write;
-  bit mem_read;
-  bit mem_write;
-  bit branch;
-  bit branch_neq;
-  bit alu_op[3];
+  bit     alu_src;
+  bit     mem_to_reg;
+  bit     reg_write;
+  bit     mem_read;
+  bit     mem_write;
+  bit     branch;
+  bit     branch_neq;
+  uint8_t alu_op;
 } control_signals;
 
 typedef struct {
