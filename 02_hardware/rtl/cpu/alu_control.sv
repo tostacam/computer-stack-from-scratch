d2c252ae640ba7c6feac6e843284a3d49c11bd31
tostacam <edgar-tostado@hotmail.com>
2026-07-24 21:35:55 -0700

    adding rest of alu control codes

--- 02_hardware/rtl/cpu/alu_control.sv
+++ 02_hardware/rtl/cpu/alu_control.sv
@@ -1,7 +1,7 @@
 `include "alu_ops.svh"
 
 module alu_control(
-  input  logic [1:0] alu_op,
+  input  logic [2:0] alu_op,
   input  logic [2:0] funct3,
   input  logic [6:0] funct7,
   output logic [3:0] alu_control  
@@ -10,25 +10,63 @@ module alu_control(
 always_comb begin
   alu_control = `ALU_OP_ADD;
   case (alu_op)
-    // (0,0) -> add
-    2'b00:
-      alu_control = `ALU_OP_ADD;
-    // (0,1) -> sub
-    2'b01:
-      alu_control = `ALU_OP_SUB;
-    // (1,0) -> decode funct3/funct7
-    2'b10: begin
-      if (funct3 == 3'b000 && funct7 == 7'b0000000)
-        alu_control = `ALU_OP_ADD;
-      if (funct3 == 3'b000 && funct7 == 7'b0100000)
-        alu_control = `ALU_OP_SUB;
+    
+    // 000 -> ADD
+    3'b000:
+      alu_control = `ALU_OP_ADD;  // ADD
+    
+    // 001 -> R-TYPE decode
+    3'b001: begin
+      case (funct3)
+        3'b000: alu_control = (funct7 == 7'b0100000) ? `ALU_OP_SUB : `ALU_OP_ADD; // ADD, SUB
+        3'b001: alu_control = `ALU_OP_SLL;                                        // SLL
+        3'b010: alu_control = `ALU_OP_SLT;                                        // SLT
+        3'b011: alu_control = `ALU_OP_SLTU;                                       // SLTU
+        3'b100: alu_control = `ALU_OP_XOR;                                        // XOR
+        3'b101: alu_control = (funct7 == 7'b0100000) ? `ALU_OP_SRA : ALU_OP_SRL;  // SRL, SRA
+        3'b110: alu_control = `ALU_OP_OR;                                         // OR
+        3'b111: alu_control = `ALU_OP_AND;                                        // AND
+      endcase
+    end
+
+    // 010 -> BRANCH compare
+    3'b010: begin
+      case (funct3)
+        3'b000,                             // BEQ
+        3'b001: alu_control = `ALU_OP_SUB;  // BNE
+        3'b100,                             // BLT
+        3'b101: alu_control = `ALU_OP_SLT;  // BGE
+        3'b110,                             // BLTU
+        3'b111: alu_control = `ALU_OP_SLTU; // BGEU
+      endcase
+    end 
+
+    // 011 -> I-type decode
+    3'b011: begin
+      case (funct3)
+        3'b000: alu_control = `ALU_OP_ADD;                                        // ADDI
+        3'b001: alu_control = `ALU_OP_SLL;                                        // SLLI
+        3'b010: alu_control = `ALU_OP_SLT;                                        // SLTI
+        3'b011: alu_control = `ALU_OP_SLTU;                                       // SLTUI
+        3'b100: alu_control = `ALU_OP_XOR;                                        // XORI
+        3'b101: alu_control = (funct7 == 7'b0100000) ? `ALU_OP_SRA : `ALU_OP_SRL; // SRLI, SRAI
+        3'b110: alu_control = `ALU_OP_OR;                                         // ORI
+        3'b111: alu_control = `ALU_OP_AND;                                        // ANDI
     end 
     
-    // (1,1) -> LUI
-    2'b11:
+    // 100 -> LUI
+    3'b100:
       alu_control = `ALU_OP_PASS_B;
-  endcase 
 
+    // 101 -> AUIPC
+    3'b101:
+      alu_control = `ALU_OP_ADD;
+
+    // 110 -> JAL/JALR
+    3'b110: 
+      alu_control = `ALU_OP_ADD;
+
+  endcase 
 end
 
 endmodule
--- 02_hardware/rtl/cpu/alu_ops.svh
+++ 02_hardware/rtl/cpu/alu_ops.svh
@@ -1,16 +1,16 @@
 `ifndef ALU_OPS_SVH
 `define ALU_OPS_SVH
 
-`define ALU_OP_ADD     4'd0
-`define ALU_OP_SUB     4'd1
-`define ALU_OP_AND     4'd2
-`define ALU_OP_OR      4'd3
-`define ALU_OP_XOR     4'd4
-`define ALU_OP_SLL     4'd5
-`define ALU_OP_SRL     4'd6
-`define ALU_OP_SRA     4'd7
-`define ALU_OP_SLT     4'd8
-`define ALU_OP_SLTU    4'd9
-`define ALU_OP_PASS_B  4'd10
+`define ALU_OP_ADD     4'b0000
+`define ALU_OP_SUB     4'b0001
+`define ALU_OP_AND     4'b0010
+`define ALU_OP_OR      4'b0011
+`define ALU_OP_XOR     4'b0100
+`define ALU_OP_SLL     4'b0101
+`define ALU_OP_SRL     4'b0110
+`define ALU_OP_SRA     4'b0111
+`define ALU_OP_SLT     4'b1000
+`define ALU_OP_SLTU    4'b1001
+`define ALU_OP_PASS_B  4'b1010
 
 `endif
