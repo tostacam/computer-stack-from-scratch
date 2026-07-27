bfc6e004ef09fd33716681c4dd01327938aa962c
tostacam <edgar-tostado@hotmail.com>
2026-07-26 18:25:49 -0700

    creating interface to run program/hex files and output json files

---
+++ 01_software/sim/tools/run_cpu.c
@@ -0,0 +1,72 @@
+#ifndef RUN_CPU_H
+#define RUN_CPU_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "cpu.h"
+
+void load_program(ROM *rom, const char *filename);
+void CPU_run(CPU *cpu);
+void output_results(CPU *cpu, const char *filename);
+
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    printf("Need input/output file: <program.c> <filename.hex> <filename.json>\n");
+    return 1;
+  }
+
+  // ROM load
+  ROM rom;
+  load_program(&rom, argv[1]);
+
+  // CPU run
+  CPU cpu;
+  CPU_init(&cpu, &rom);
+  CPU_run(&cpu);
+
+  // Output
+  output_results(&cpu, argv[2]);
+}
+
+void load_program(ROM *rom, const char *filename) {
+  FILE *fp = fopen(filename, "r");
+  assert(fp);
+
+  uint32_t instruction, i = 0;
+
+  while (fscanf(fp, "%x", &instruction) == 1) {
+    rom->bytes[i++] = instruction & 0xFF;
+    rom->bytes[i++] = (instruction >> 8) & 0xFF;
+    rom->bytes[i++] = (instruction >> 16) & 0xFF;
+    rom->bytes[i++] = (instruction >> 24) & 0xFF;
+  }
+  rom->size = i / 4;
+
+  fclose(fp);
+}
+
+void CPU_run(CPU *cpu) {
+  while (decode_amount(register64_output(&cpu->pc.output_reg)) < cpu->rom.size * 4) {
+    CPU_cycle(cpu);
+  }
+}
+
+void output_results(CPU *cpu, const char* filename) {
+  FILE *fp = fopen(filename, "w");
+
+  fprintf(fp, "{\n");
+  fprintf(fp, "  \"pc\": %llu,\n", decode_amount(register64_output(&cpu->pc.output_reg)));
+  fprintf(fp, "  \"registers\": {\n");
+  for (int i = 0; i < 32; ++i) {
+    fprintf(fp, "    \"x%d\": %llu%s\n", i, decode_amount(register64_output(&cpu->rf.register_data[i])), (i == 31) ? "" : ",");
+  }
+  fprintf(fp, "  },\n");
+  fprintf(fp, "  \"memory\": {\n");
+  fprintf(fp, "    \"0x0000\": %d\n", cpu->ram.bytes[0] | (cpu->ram.bytes[1] << 8) | (cpu->ram.bytes[2] << 16) | (cpu->ram.bytes[3] << 24));
+  fprintf(fp, "  }\n");
+  fprintf(fp, "}\n");
+
+  fclose(fp);
+}
+
+#endif
