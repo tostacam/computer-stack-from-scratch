349406ccf2059fd3c9df7e7d7f3020dba60b8d03
tostacam <edgar-tostado@hotmail.com>
2026-08-03 14:25:33 -0700

    adding rtl test, comparison logic is generic now

--- 03_validation/run_tests.py
+++ 03_validation/run_tests.py
@@ -5,7 +5,7 @@ import json
 ROOT = Path(__file__).resolve().parent.parent
 ASSEMBLER = ROOT / "01_software" / "asm" / "assembler.py"
 SIM       = ROOT / "01_software" / "sim" / "tools" / "run_cpu"
-RTL       = ROOT / "02_hardware" / "rtl" / "verilator_cpu"
+RTL       = ROOT / "02_hardware" / "rtl" / "obj_dir" / "Vcpu"
 
 PROGRAMS  = ROOT / "03_validation" / "programs"
 HEX_FILES = ROOT / "03_validation" / "hex_files"
@@ -27,30 +27,32 @@ def run_sim(base):
   ]) 
 
 def run_rtl(base):
-  pass
+  subprocess.run([
+    RTL,
+    f"+ROM={HEX_FILES}/{base}.hex",
+    RESULTS / f"{base}.rtl.json"
+  ])
 
-def compare(base):
-  expected = json.load(open(PROGRAMS/f"{base}.expected.json"))
-  sim      = json.load(open(RESULTS/f"{base}.sim.json"))
-
-  test_passed = True
+def compare(expected_file, result_file):
+  expected = json.load(open(expected_file))
+  result   = json.load(open(result_file))
 
   # PC
   if "pc" in expected:
-    if sim["pc"] != expected["pc"]: 
-      test_passed = False
+    if result["pc"] != expected["pc"]: 
+      return False
 
   # Registers
   for reg, val in expected.get("registers", {}).items():
-    if sim["registers"][reg] != val:
-      test_passed = False
+    if result["registers"][reg] != val:
+      return False
 
   # Memory
   for addr, value, in expected.get("memory", {}).items():
-    if sim["memory"][addr] != val:
-      test_passed = False
+    if result["memory"][addr] != val:
+      return False
 
-  return test_passed
+  return True
 
 def main():
   # creating folders
@@ -64,19 +66,35 @@ def main():
   for test in PROGRAMS.glob("*.s"):
     base = test.stem
 
+    # asm
     assemble(base)
+
+    # sim
     run_sim(base)
-    #run_rtl(base)
-    
-    passed = compare(base)
+    sim_pass = compare(
+      PROGRAMS / f"{base}.expected.json", 
+      RESULTS / f"{base}.sim.json")
+
     tests_total += 1
-
-    if passed:
+    if sim_pass:
       tests_passed += 1
-      print(f"PASS {base}")
+      print(f"PASS [SIM] {base}")
     else:
-      print(f"FAIL {base}")
+      print(f"FAIL [SIM] {base}")
 
+    # rtl
+    run_rtl(base)
+    rtl_pass = compare(
+      PROGRAMS / f"{base}.expected.json", 
+      RESULTS / f"{base}.rtl.json")
+    
+    tests_total += 1
+    if rtl_pass:
+      tests_passed += 1
+      print(f"PASS [RTL] {base}")
+    else:
+      print(f"FAIL [RTL] {base}")
+  
   percentage = 100 * tests_passed / tests_total
   print(f"Summary: {tests_passed}/{tests_total} tests passed ({percentage:.1f}%)")
 
