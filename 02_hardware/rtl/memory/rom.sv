4366dd1b15b0f03b7f27c9e0b75faa46855e5780
tostacam <edgar-tostado@hotmail.com>
2026-08-03 13:22:23 -0700

    reading external rom file

--- 02_hardware/rtl/memory/rom.sv
+++ 02_hardware/rtl/memory/rom.sv
@@ -1,6 +1,5 @@
 module rom #(
-  parameter MEM_SIZE = 1024,
-  parameter ROM_FILE = "tools/add.hex"
+  parameter MEM_SIZE = 1024
 )(
   input  logic [63:0] address,
   output logic [31:0] instruction
@@ -8,8 +7,13 @@ module rom #(
 
 logic [31:0] memory [0:MEM_SIZE-1];
 
+string rom_file;
+
 initial begin
-  $readmemh(ROM_FILE, memory);
+  if (!$value$plusargs("ROM=%s", rom_file))
+    rom_file = "tools/add.hex"; // default, just in case
+  $display("Loading ROM: %s", rom_file);
+  $readmemh(rom_file, memory);
 end 
 
 localparam ADDR_WIDTH = $clog2(MEM_SIZE);
