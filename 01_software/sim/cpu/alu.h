af6c20e3d8bf90185157f3e52718d06840936b36
tostacam <edgar-tostado@hotmail.com>
2026-08-06 09:08:58 -0700

    small format fix

--- 01_software/sim/cpu/alu.h
+++ 01_software/sim/cpu/alu.h
@@ -13,18 +13,14 @@
 enum alu_op {
   ALU_OP_ADD,
   ALU_OP_SUB,
-
   ALU_OP_AND,
   ALU_OP_OR,
   ALU_OP_XOR,
-
   ALU_OP_SLL,
   ALU_OP_SRL,
   ALU_OP_SRA,
-
   ALU_OP_SLT,
   ALU_OP_SLTU,
-  
   ALU_OP_PASS_B
 };
 
