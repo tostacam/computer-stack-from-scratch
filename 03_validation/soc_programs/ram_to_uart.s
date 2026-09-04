484246181da631f9211401ef85593b6272147d0c
tostacam <edgar-tostado@hotmail.com>
2026-09-04 12:57:11 -0700

    adding infinite loop to uart tests

--- 03_validation/soc_programs/ram_to_uart.s
+++ 03_validation/soc_programs/ram_to_uart.s
@@ -1,8 +1,10 @@
-addi x1, x0, 123
-lui  x2, 0x10
-sw   x1, 0(x2)
-lui  x2, 0x20000  # UART
-addi x2, x2, 0x10
-addi x1, x0, 65
-sb   x1, 0(x2)
-ebreak
+  addi x1, x0, 123
+  lui  x2, 0x10
+  sw   x1, 0(x2)
+  lui  x2, 0x20000  # UART
+  addi x2, x2, 0x10
+  addi x1, x0, 65
+  sb   x1, 0(x2)
+
+loop:
+  jal x0, loop
--- 03_validation/soc_programs/uart.s
+++ 03_validation/soc_programs/uart.s
@@ -1,5 +1,7 @@
-lui  x1, 0x20000
-addi x1, x1, 0x10
-addi x2, x0, 65
-sb   x2, 0(x1)
-ebreak
+  lui  x1, 0x20000
+  addi x1, x1, 0x10
+  addi x2, x0, 65
+  sb   x2, 0(x1)
+
+loop:
+  jal x0, loop
